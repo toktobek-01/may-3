521c980159842ea1c541d674f077798bda3551ae
toktobek-01 <toktobek.d@gmail.com>
2025-05-03 21:20:32 -0500

    f

---
+++ src/zoo.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class zoo {
+    public void makeSound() {
+    }
+
+    class Animal { // default access class
+        void makeSound() { // default access method
+            System.out.println("Animal makes sound");
+        }
+    }
+
+    }
+
---
+++ src/zookeeper.java
@@ -0,0 +1,7 @@
+public class zookeeper {
+        public static void main(String[] args) {
+            zoo a = new zoo(); // works fine: same file & same default package
+            a.makeSound();
+        }
+    }
+
